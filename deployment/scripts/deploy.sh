71b5c650e146c3288a72a674efb3bd441fbc3832
Axl <jsuarezb99@gmail.com>
2026-04-22 19:49:11 -0500

    updated scripts

--- deployment/scripts/deploy.sh
+++ deployment/scripts/deploy.sh
@@ -26,18 +26,15 @@ set -e
 
 echo "=== set up inicial ==="
 echo "dando permisos de ejecucion"
-
-chmod -x 400 start_server.sh
-chmod -x 400 start_login.sh
 # Actualizar e instalar dependencias sistema
 echo "Actualizando e instalando dependencias..."
-sudo apt update -y
-sudo apt install -y build-essential
-sudo apt install -y libsqlite3-dev
+# apt update -y
+apt install -y build-essential
+apt install -y libsqlite3-dev
 
 # Instalar tmux  
 echo "Installing netcat and tmux..."
-sudo apt install -y tmux
+apt install -y tmux
 
 
 # Compilar server y login service
@@ -47,11 +44,8 @@ make
 cd ../Login_service
 make
 
-# dar permisos de ejecucion a los ejecutables
-chmod -x 400 login_service
-cd ../server
-chmod -x 400 server
-
 echo "set up inicial completado!"
 
 echo "abriendo tmux (my_servers)..."
+cd ../deployment/scripts
+tmux new -s my_servers
